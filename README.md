d8e4f7752bb77124306e0dbde88b63b1a0ab9ae8
JP <juan.pablo.lb99@gmail.com>
2021-11-07 13:53:23 -0600

    Add auth service

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ npx ng g c home
 npx ng g c login
 
 npx ng g g guards/auth
+npx ng g s services/auth
+
+npx ng g m modules/products --route products -m app
 ```
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  getToken(): boolean {
+    // api call
+    // store token
+
+    return false;
+  }
+
+}
