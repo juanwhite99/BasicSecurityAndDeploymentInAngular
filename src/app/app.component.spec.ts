ed9852832f21254faef5bca86c24a954e98325e7
JP <juan.pablo.lb99@gmail.com>
2021-11-07 15:07:12 -0600

    Unit testing of home component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('security-test-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('security-test-app app is running!');
-  });
 });
